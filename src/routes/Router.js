0f38345cd4932899ca004f79ec62bc8e36d78946
cleooo5857 <zleooo@naver.com>
2023-09-01 23:18:39 +0900

    Build : react-router-dom 라이브러리 설치

---
+++ src/routes/Router.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* path = 경로  element = 컴포넌트*/}
+        {/* <Route path="/" element={<HomePage />} /> */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default Router;

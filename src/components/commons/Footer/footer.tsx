fac2de85ee11e79a90615a57658ba1be09e9dd76
cleooo5857 <zleooo@naver.com>
2023-09-02 10:58:30 +0900

    Design :공 용 레이아웃 푸터추가

--- src/components/commons/Footer/footer.tsx
+++ src/components/commons/Footer/footer.tsx
@@ -3,7 +3,7 @@ import * as S from './style';
 const Footer = () => {
   return (
     <S.FooterWrapper>
-      <S.CopyWrite>Copyright 2023. CStudy All rights reserved </S.CopyWrite>
+      <S.CopyWrite>Copyright 2023. CStudy All rights reserveds </S.CopyWrite>
       <a href="https://github.com/CS-tudy/CStudy_FrontEnd" target="_blank"></a>
     </S.FooterWrapper>
   );

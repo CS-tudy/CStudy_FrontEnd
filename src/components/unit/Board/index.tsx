92ac8073d910e51646a560b503c83a46867219c0
hannaax <hanan941008@gmail.com>
2023-09-06 12:36:42 +0900

    Fix: auth api 및 modal 관련 수정

--- src/components/unit/Board/index.tsx
+++ src/components/unit/Board/index.tsx
@@ -1,7 +1,32 @@
 import Container from 'components/commons/Container';
+import Modal from 'components/unit/Modal';
+import useModal from 'hooks/useModal';
+import * as S from './style';
 
 const BoardList = () => {
-  return <Container>보드 게시판</Container>;
+  const { modalIsOpen, toggleModal } = useModal();
+
+  const openModal = () => {
+    toggleModal();
+  };
+
+  return (
+    <>
+      {modalIsOpen && (
+        <Modal toggleModal={toggleModal}>
+          <S.ModalTest>test</S.ModalTest>
+        </Modal>
+      )}
+      <Container>
+        <div>
+          <p>
+            <button onClick={openModal}>modalTest</button>
+          </p>
+          <p>보드 게시판</p>
+        </div>
+      </Container>
+    </>
+  );
 };
 
 export default BoardList;
--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -1,36 +1,8 @@
 import BoardList from 'components/unit/Board';
-import Modal from 'components/unit/Modal';
-import useModal from 'hooks/useModal';
-import { styled } from 'styled-components';
-import { useEffect } from 'react';
-
-const ModalTest = styled.div`
-  position: fixed;
-  top: 20vh;
-  left: 5%;
-  width: 50%;
-  background-color: white;
-  padding: 1rem;
-  border-radius: 14px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
-  z-index: 30;
-  animation: slide-down 300ms ease-out forwards;
-`;
 
 const Board = () => {
-  const { modalIsOpen, toggleModal } = useModal();
-  useEffect(() => {
-    setTimeout(() => {
-      toggleModal();
-    }, 0);
-  }, []);
   return (
     <>
-      {modalIsOpen && (
-        <Modal toggleModal={toggleModal}>
-          <ModalTest>test</ModalTest>
-        </Modal>
-      )}
       <BoardList />
     </>
   );

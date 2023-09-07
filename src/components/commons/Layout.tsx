5d6b46888e0ebfde663d6b14f75b5a2bc2e3fce7
cleooo5857 <zleooo@naver.com>
2023-09-07 17:13:55 +0900

    Design: 헤더 레이아웃 수

--- src/components/commons/Layout.tsx
+++ src/components/commons/Layout.tsx
@@ -1,15 +1,22 @@
+import { styled } from 'styled-components';
 import Container from './Container';
 import Footer from './Footer/footer';
 import Header from './Header/Header';
 
 const Layout = ({ children }: React.PropsWithChildren) => {
   return (
-    <Container>
-      <Header />
-      {children}
-      <Footer />
-    </Container>
+    <>
+      <Wrapper>
+        <Header />
+        {children}
+        <Footer />
+      </Wrapper>
+    </>
   );
 };
 
 export default Layout;
+
+const Wrapper = styled.div`
+  position: relative;
+`;

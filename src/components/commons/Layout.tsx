63e55e81a24e1ed6e289219b80efbd66998c7f5e
hannaax <hanan941008@gmail.com>
2023-09-08 11:12:51 +0900

    Feat: SignModal, ConfirmModal 구현 (#7)

--- src/components/commons/Layout.tsx
+++ src/components/commons/Layout.tsx
@@ -1,13 +1,14 @@
-import Container from './Container';
 import Footer from './Footer/footer';
 import Header from './Header/Header';
 
 const Layout = ({ children }: React.PropsWithChildren) => {
   return (
     <>
-      <Header />
-      {children}
-      <Footer />
+      <div>
+        <Header />
+        {children}
+        <Footer />
+      </div>
     </>
   );
 };

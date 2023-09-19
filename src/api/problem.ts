1e7fe37589860dd8a4730151fcb4152f76e4c08a
kim sun hong <kongsunk@nate.com>
2023-09-19 18:56:19 +0900

    feat: Admin문제생성 추가

--- src/api/problem.ts
+++ src/api/problem.ts
@@ -1,7 +1,6 @@
 import { instance } from 'api';
 import { FieldValues } from 'react-hook-form';
 
-// 문제 생성하기 & 대량 문제 생성하기
 export const problemSet = async (formData: FieldValues) => {
   const response = await instance.post('/api/questions', formData);
 

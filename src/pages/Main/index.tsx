5a99468ca28e79375063a0a962559fced9ac70dd
kim sun hong <kongsunk@nate.com>
2023-09-19 18:59:28 +0900

    Design:배너 스켈레톤ui  설정

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -8,45 +8,18 @@ const Banner = lazy(() => import('../../components/main/Banner'));
 import * as S from './style';
 import SignInModal from 'components/unit/SignIn';
 import { useSelector } from 'react-redux';
+import { Suspense, lazy } from 'react';
+import Skeleton from 'components/commons/Skeleton';
+const Banner = lazy(() => import('../../components/main/Banner'));
 const Main = () => {
-  // const isAuthenticated = useSelector(
-  //   (state: any) => state.auth.isAuthenticated,
-  // );
-
   return (
     <S.Main>
-      <Banner />
+      <Suspense fallback={<Skeleton />}>
+        <Banner />
+      </Suspense>
       <Contents />
     </S.Main>
   );
 };
 
 export default Main;
-
-{
-  /* <div>
-        <h2>버튼 컴포넡트</h2>
-        <div>
-          <div>
-            <Button variant="primary" size="full">
-              1
-            </Button>
-          </div>
-          <div>
-            <Button variant="primary" size="small">
-              2
-            </Button>
-          </div>
-          <div>
-            <Button variant="primary" size="medium">
-              3
-            </Button>
-          </div>
-          <div>
-            <Button variant="primary" size="large">
-              5
-            </Button>
-          </div>
-        </div>
-      </div> */
-}

a006a6ffddb67fac7ed9a3184e364a02de859cf6
kim sun hong <kongsunk@nate.com>
2023-09-20 21:42:33 +0900

    feat: myPage 비밀번호변경 api 추가

--- src/api/mypage.ts
+++ src/api/mypage.ts
@@ -1,7 +1,13 @@
 import { instance } from 'api';
+import { FieldValues } from 'react-hook-form';
 import { MyPage } from 'types/api';
 
 export const getMyPage = async (): Promise<MyPage> => {
   const response = await instance.get(`/api/mypage`);
+  return response.data;
+};
+
+export const PwdEdit = async (formData: FieldValues) => {
+  const response = await instance.put('api/member/member', formData);
   return response.data;
 };

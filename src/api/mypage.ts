1f04454b29ad14eda7cb6c133bebbb0ada3a0c2d
kim sun hong <kongsunk@nate.com>
2023-10-01 20:48:32 +0900

    feat: mypage api추가

--- src/api/mypage.ts
+++ src/api/mypage.ts
@@ -1,6 +1,5 @@
 import { instance } from 'api';
 import { FieldValues } from 'react-hook-form';
-import { MyPage } from 'types/api';
 
 // 내정보
 export const getMyPage = async () => {
@@ -12,5 +11,35 @@ export const getMyPage = async () => {
 // 비밀번호변경
 export const PwdEdit = async (formData: FieldValues) => {
   const response = await instance.put('api/member/member', formData);
+  return response.data;
+};
+
+// s3 버킷에 IAM 파일 업로드
+export const ImgUpload = async (formData: FieldValues) => {
+  const response = await instance.post(
+    `api/member/upload?multipartFileList=`,
+    formData,
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    },
+  );
+  return response.data;
+};
+
+// S3 버켓에서 사진 가져오기
+export const ImgDownload = async () => {
+  const response = await instance.get('api/member/download', {
+    responseType: 'blob',
+  });
+  return response.data;
+};
+
+// 승인현황
+export const ApprovalStatus = async () => {
+  const response = await instance.get(
+    `api/request/member?page=10&size=1&sort=`,
+  );
   return response.data;
 };

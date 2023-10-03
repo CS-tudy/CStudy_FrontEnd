e68c5195b4736ff5118a08a7dc14381134cb543e
kim sun hong <kongsunk@nate.com>
2023-10-03 18:26:32 +0900

    🎨:art: 코드구조개선

--- src/hooks/mypage/useGetImg.ts
+++ src/hooks/mypage/useGetImg.ts
@@ -1,14 +1,15 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ImgDownload } from 'api/mypage';
 
-export const useGetImg = createAsyncThunk('myPage/useGetImg', async () => {
-  try {
-    const data = await ImgDownload();
-    const url = URL.createObjectURL(data);
-    return url;
-  } catch (error) {
-    // 오류 처리
-    console.log(error);
-    throw new Error('파일 다운로드 중 오류 발생');
-  }
-});
+export const useGetImg = createAsyncThunk(
+  'myPageDownload/useGetImg',
+  async () => {
+    try {
+      const data = await ImgDownload();
+      const url = URL.createObjectURL(data);
+      return url;
+    } catch (error) {
+      throw new Error('파일 다운로드 중 오류 발생');
+    }
+  },
+);

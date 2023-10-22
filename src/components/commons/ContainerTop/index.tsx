f219717e0ac4ad3e69d6d4d631351c892e8e8c14
hannaax <hanan941008@gmail.com>
2023-10-22 22:09:17 +0900

    design: ContainerTop, ContainerBottom 적용

---
+++ src/components/commons/ContainerTop/index.tsx
@@ -0,0 +1,7 @@
+import * as S from './style';
+
+const ContainerTop = ({ children }: React.PropsWithChildren) => {
+  return <S.ContainerTop>{children}</S.ContainerTop>;
+};
+
+export default ContainerTop;

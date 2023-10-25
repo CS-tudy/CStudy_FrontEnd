21e0fc6fd262b846c9b74e36e9b23f6131c0c6df
kim sun hong <kongsunk@nate.com>
2023-10-25 19:59:16 +0900

    feat: required추가

--- src/components/unit/CreateProblem/CreateProblemInput/index.tsx
+++ src/components/unit/CreateProblem/CreateProblemInput/index.tsx
@@ -35,6 +35,7 @@ const CreateProblemInput = ({
   onChange,
   checked,
   errors,
+  required,
 }: CreateProblemInputProps) => {
   return (
     <>
@@ -47,6 +48,7 @@ const CreateProblemInput = ({
             type={type}
             {...register(resgiterName)}
             {...errors}
+            required
           />
         </>
       )}
@@ -59,6 +61,7 @@ const CreateProblemInput = ({
           name={name}
           {...errors}
           {...(register(resgiterName), { onChange: () => onChange })}
+          required
         />
       )}
     </>

b492041c338d2561fd403fc1ece74a2bbec2580b
kim sun hong <kongsunk@nate.com>
2023-10-31 18:53:01 +0900

    Design:main content link 위치 수정

--- src/components/unit/main/Contents/style.ts
+++ src/components/unit/main/Contents/style.ts
@@ -109,7 +109,7 @@ export const Texts = styled.span`
   display: inline-block;
   font-size: ${FONT.REGULAR_14};
   line-height: 2.5rem;
-  margin: 1rem 3rem;
+  margin: 1rem 0 3rem;
   ${media.tablet} {
     font-size: ${FONT.REGULAR_12};
   }

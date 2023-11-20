6a4a1c9c622aa82b6ac6b78ff84c49b5b11bd128
cleooo5857 <zleooo@naver.com>
2023-11-20 14:29:19 +0900

    test : Oauth 배포 환경 테스트

--- src/pages/Admin/OauthRedirect/index.tsx
+++ src/pages/Admin/OauthRedirect/index.tsx
@@ -1,4 +1,3 @@
-import { instance } from 'api';
 import { login } from 'hooks/@redux/authSlice';
 import Cookies from 'js-cookie';
 import Toast from 'libs/Toast';
@@ -7,26 +6,19 @@ import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { userStorage } from 'repository/userStorage';
 
-const OAuthRedirect = async () => {
+const OAuthRedirect = () => {
   const dispatch = useDispatch();
 
-  // const accessToken = Cookies.get('accessToken');
-  // const refreshToken = Cookies.get('refreshToken');
-  const response = await instance.get('/oauth2/login');
-  const cookies = response.headers['set-cookie'];
-  const cookies1 = response.headers['accessToken'];
-  const cookies2 = response.headers['refreshToken'];
-  console.log('1', cookies);
-  console.log('2', cookies1);
-  console.log('3', cookies2);
+  const accessToken = Cookies.get('accessToken');
+  const refreshToken = Cookies.get('refreshToken');
 
-  // if (!accessToken || !refreshToken)
-  //   return Toast.error('로그인에 실패했습니다.');
+  if (!accessToken || !refreshToken)
+    return Toast.error('로그인에 실패했습니다.');
 
-  // userStorage.set({
-  //   accessToken,
-  //   refreshToken,
-  // });
+  userStorage.set({
+    accessToken,
+    refreshToken,
+  });
 
   dispatch(login());
 

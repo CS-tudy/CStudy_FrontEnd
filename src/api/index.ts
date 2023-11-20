3af1394dcc8bb628fba969b8e23a8632648ee687
hannaax <hanan941008@gmail.com>
2023-11-20 12:11:06 +0900

    feat: axios withCredentials true 설정

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,6 +5,7 @@ import { retryToken } from './auth';
 
 export const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  withCredentials: true,
 });
 
 instance.interceptors.request.use(

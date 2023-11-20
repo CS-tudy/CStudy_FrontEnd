3826ffd4febe1112cc5f90e5cb6a1db9f30895bd
cleooo5857 <zleooo@naver.com>
2023-11-20 14:14:35 +0900

    test : Oauth 배포 환경 테스트

--- src/pages/Admin/OauthRedirect/index.tsx
+++ src/pages/Admin/OauthRedirect/index.tsx
@@ -1,3 +1,4 @@
+import { instance } from 'api';
 import { login } from 'hooks/@redux/authSlice';
 import Cookies from 'js-cookie';
 import Toast from 'libs/Toast';
@@ -6,25 +7,30 @@ import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { userStorage } from 'repository/userStorage';
 
-const OAuthRedirect = () => {
+const OAuthRedirect = async () => {
   const dispatch = useDispatch();
 
-  const accessToken = Cookies.get('accessToken');
-  const refreshToken = Cookies.get('refreshToken');
+  // const accessToken = Cookies.get('accessToken');
+  // const refreshToken = Cookies.get('refreshToken');
+  const response = await instance.get('/oauth2/login');
+  const cookies = response.headers['set-cookie'];
+  const cookies1 = response.headers['accessToken'];
+  const cookies2 = response.headers['refreshToken'];
+  console.log('1', cookies);
+  console.log('2', cookies1);
+  console.log('3', cookies2);
 
-  if (!accessToken || !refreshToken)
-    return Toast.error('로그인에 실패했습니다.');
+  // if (!accessToken || !refreshToken)
+  //   return Toast.error('로그인에 실패했습니다.');
 
-  userStorage.set({
-    accessToken,
-    refreshToken,
-  });
+  // userStorage.set({
+  //   accessToken,
+  //   refreshToken,
+  // });
 
   dispatch(login());
 
-  window.location.replace(
-    `http://cstudy-projects.s3-website.ap-northeast-2.amazonaws.com`,
-  );
+  window.location.replace(`https://cstudying.site`);
 
   return <div>Login...</div>;
 };
